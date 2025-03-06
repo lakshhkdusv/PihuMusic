518f8c6b4bd04f897b481344e10ac44170afd6c2
Shruti1447 <radhashinde2574@gmail.com>
2025-03-07 02:12:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ BOT_USERNAME = getenv("BOT_USERNAME" , "Netflix_Musicbot")
 # --------------------------------------------------------
 BOT_NAME = getenv("BOT_NAME" , "Netflix Music")
 # ---------------------------------------------------------
-ASSUSERNAME = getenv("ASSUSERNAME" , "Netflix Assistant")
+ASSUSERNAME = getenv("ASSUSERNAME" , "Asistant_Netflix")
 # ---------------------------------------------------------
 
 
